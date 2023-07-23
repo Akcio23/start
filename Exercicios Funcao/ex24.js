e8d7c8d789bf5d37a2b3624c715c5709fb1ea1a3
Pedro <dev.akcio@gmail.com>
2023-07-22 22:06:56 -0300

    22/07

---
+++ Exercicios Funcao/ex24.js
@@ -0,0 +1,7 @@
+/*24) Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.*/
+function contador () {
+    for (let i = 1; i<=50; i++) {
+        console.log(i);
+    }
+}
+contador()
